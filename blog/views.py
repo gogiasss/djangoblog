1a36fea3fe4e95105f669fcbf7a2b47695941031
Rus <valiev.ruslan@gmail.com>
2020-07-31 23:42:07 +0500

    update blog page, nav menu

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def showblog(request):
+	post = Post.objects
+	return render(request, 'blog/blog.html', {'post': post})

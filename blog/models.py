e11971740676d641015272e61d478580ece2fd8d
Rus <valiev.ruslan@gmail.com>
2020-07-31 14:20:19 +0500

    add events template

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 # Create your models here.
 class Post(models.Model):
-	image = models.ImageField(upload_to='blog_images/')
-	text = models.TextField()
 	title = models.CharField(max_length=300)
 	date = models.DateField()
+	text = models.TextField()
+	image = models.ImageField(upload_to='blog_images/')

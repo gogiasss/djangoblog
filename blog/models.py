09830dd6c70d3514c7b2888595593eebb5bbd68f
Rus <valiev.ruslan@gmail.com>
2020-07-31 12:47:30 +0500

    rename blog to post

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
-class Blog(models.Model):
-	blog_image = models.ImageField(upload_to='blog_images/')
-	blog_text = models.CharField(max_length=3000)
-	blog_title = models.CharField(max_length=30)
-	blog_date = models.DateField()
+class Post(models.Model):
+	image = models.ImageField(upload_to='blog_images/')
+	text = models.TextField()
+	title = models.CharField(max_length=300)
+	date = models.DateField()
